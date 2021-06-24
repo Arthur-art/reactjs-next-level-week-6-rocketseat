60ff24ea8e1d5d41d2f88663085b136f0cda7206
Arthur-art <arthurteixeira.guts@gmail.com>
2021-06-24 14:36:48 -0300

    Trabalhando com before e after scss no component Home

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -15,14 +15,14 @@ export const Home = () => {
                     <strong>{"Crie salas de Q&A ao-vivo"}</strong>
                     <p>Tire dúvidas da sua audiência em tempo real</p>
                 </aside>
-                <main className="main-content">
-                    <div>
+                <main>
+                    <div className="main-content">
                         <img src={logoImg} alt="Letmeask" />
                         <button>
                             <img src={googleImg} alt="GoogleImg" />
                             Crie sua sala com o Google
                         </button>
-                        <div>ou entre em uma sala</div>
+                        <div className="separator">ou entre em uma sala</div>
                         <form>
                             <input type="text" placeholder="Digite o código da sala" />
                             <button type="submit">Entrar na sala</button>
