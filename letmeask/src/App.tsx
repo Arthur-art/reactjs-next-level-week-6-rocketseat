fad3c57c570d188268a61b0f76ab573d47cfb14f
Arthur-art <arthurteixeira.guts@gmail.com>
2021-06-24 09:55:03 -0300

    Praticando useState e useEffect

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 
 export const App = ({text}:any)=>{
+    const [number, setNumber] = useState(0);
+
+    const handleNumber = ()=>{
+        setNumber((prev)=>(prev+1))
+    }
+    useEffect(()=>{
+        if(number>9){
+            alert("O limite é 10")
+        }
+    },[number])
+
     return (
         <>
             <h1>{text || "Hello World" }</h1>
+            <button onClick={()=>handleNumber()}>{number}</button>
         </>
     )
 }
