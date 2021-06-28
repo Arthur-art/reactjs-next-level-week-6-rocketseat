b05930a14acbd2b9bac0b6d1d8857099db9fc975
Arthur-art <arthurteixeira.guts@gmail.com>
2021-06-28 16:12:56 -0300

    Refatorando auth context, passando para dentro do context como childreen o component App

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,75 +1,18 @@
-import React, { createContext, useState } from 'react'
-import { firebase, auth } from './services/firebase'
+import { AuthContextProvider } from "../src/contexts/AuthContext"
 
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
 
 import { BrowserRouter, Route } from "react-router-dom"
-import { useEffect } from 'react'
-
-type User = {
-    id: string;
-    name: string | null;
-    imgProfile: any;
-}
-
-type AuthContextType = {
-    user: User | undefined;
-    signWithGoogle: () => Promise<void>;
-}
-
-export const authContextProvider = createContext({} as AuthContextType)
 
 export const App = ({ text }: any) => {
 
-    const [user, setUser] = useState<User>()
-
-    useEffect(() => {
-        auth.onAuthStateChanged(user => {
-            if (user) {
-
-                const { displayName, photoURL, uid } = user;
-
-                if (!displayName || !photoURL) {
-                    throw new Error("Conta da google invalida")
-                }
-
-                setUser({
-                    id: uid,
-                    name: displayName,
-                    imgProfile: photoURL
-                })
-            }
-
-        })
-    }, [])
-
-
-    async function signWithGoogle() {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        const result = await auth.signInWithPopup(provider);
-
-        if (result.user) {
-            const { displayName, photoURL, uid } = result.user;
-
-            if (!displayName || !photoURL) {
-                throw new Error("Conta da google invalida")
-            }
-
-            setUser({
-                id: uid,
-                name: displayName,
-                imgProfile: photoURL
-            })
-        }
-    }
-
     return (
-        <authContextProvider.Provider value={{ user, signWithGoogle }}>
-            <BrowserRouter>
+        <BrowserRouter>
+            <AuthContextProvider>
                 <Route path="/" exact component={Home} />
                 <Route path="/rooms/news" component={NewRoom} />
-            </BrowserRouter>
-        </authContextProvider.Provider>
+            </AuthContextProvider>
+        </BrowserRouter>
     )
 }
---
+++ letmeask/src/contexts/AuthContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+import {auth,firebase} from "../services/firebase"
+
+type User = {
+    id: string;
+    name: string | null;
+    imgProfile: any;
+}
+
+type AuthContextType = {
+    user: User | undefined;
+    signWithGoogle: () => Promise<void>;
+}
+
+type contextProps = {
+    children: ReactNode
+}
+
+const [user, setUser] = useState<User>()
+
+useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+        if (user) {
+
+            const { displayName, photoURL, uid } = user;
+
+            if (!displayName || !photoURL) {
+                throw new Error("Conta da google invalida")
+            }
+
+            setUser({
+                id: uid,
+                name: displayName,
+                imgProfile: photoURL
+            })
+        }
+    })
+    return () => {
+        unsubscribe()
+    }
+}, [])
+
+
+async function signWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const result = await auth.signInWithPopup(provider);
+
+    if (result.user) {
+        const { displayName, photoURL, uid } = result.user;
+
+        if (!displayName || !photoURL) {
+            throw new Error("Conta da google invalida")
+        }
+
+        setUser({
+            id: uid,
+            name: displayName,
+            imgProfile: photoURL
+        })
+    }
+}
+
+export const authContextProvider = createContext({} as AuthContextType)
+
+export const AuthContextProvider = ({children}:contextProps)=>{
+    return (
+        <authContextProvider.Provider value={{ user, signWithGoogle }}>
+            {children}
+        </authContextProvider.Provider>
+    )
+}
