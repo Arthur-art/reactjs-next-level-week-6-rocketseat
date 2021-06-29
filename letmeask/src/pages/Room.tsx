e8c1c0e7a0d0e2c83b826d53720c0527664f74aa
Arthur-art <arthurteixeira.guts@gmail.com>
2021-06-29 15:54:37 -0300

    Desenvolvendo page Room

--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,8 +1,16 @@
+import logoImg from "../assets/images/logo.svg"
+
 export const Room = ()=>{
 
     return(
-        <>
-            <h1>Room</h1>
-        </>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="letmeask" />
+                    <div>codigo</div>
+                </div>
+            </header>
+            <main></main>
+        </div>
     )
 }
